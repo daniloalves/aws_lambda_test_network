ca40665ab284709384ea9fc571e10e00490caba0
Danilo Alves <daniloha7@gmail.com>
2020-05-26 14:17:17 -0300

    Instructions

--- build.sh
+++ build.sh
@@ -6,10 +6,12 @@ help(){
     echo "* You must install module Requests, optionally boto3."
     echo "* Using virtualenv:"
     echo "virtualenv -p python3 env"
+    echo "source env/bin/activate"
     echo "pip install requests -t ./"
     echo "pip install boto3 -t ./"
     echo ""
     echo "Or, only pip install with -t ./"
+    echo "And run build.sh again."
     echo ""
 }
 
